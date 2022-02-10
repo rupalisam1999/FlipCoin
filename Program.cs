6dbee7cf64d943b53b0b5afd1aa5077d65679b8e
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-10 20:11:17 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FlipCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomCheck = random.Next(0, 2);
+            if (randomCheck == 0)
+            {
+                Console.WriteLine("HEAD");
+            }
+            else
+            {
+                Console.WriteLine("TAIL");
+
+            }
+        }
+    }
+} 
+        
+    
+
